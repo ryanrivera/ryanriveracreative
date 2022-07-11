74eaea7dcb8a87b9503801c99752ab686b1ab272
Ryan Rivera <ryancarivera@gmail.com>
2022-07-11 16:39:25 -0700

    fix build error

--- pages/_document.js
+++ pages/_document.js
@@ -4,10 +4,10 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="//fonts.googleapis.com" />
+        <link rel="preconnect" href="//fonts.gstatic.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=DM+Sans&family=Playfair+Display:wght@600&display=swap"
+          href="//fonts.googleapis.com/css2?family=DM+Sans&family=Playfair+Display:wght@600&display=swap"
           rel="stylesheet"
         />
       </Head>
